0f5583f5d9803ba24c1c83783e7edf3303f70fbf
Windisley <windisleyl@gmail.com>
2024-11-28 01:20:51 -0300

    Somando os Indices de Um Array

---
+++ Documents/Dev/Javascript/Algoritimos/sumArray/index.js
@@ -0,0 +1,20 @@
+const valuearr = document.querySelector("#valuearr")
+
+function sumArray(arr){
+   
+  let minArray = arr[0] 
+   let sum;
+    for(let i=1; i<arr.length; i++){
+    
+        sum = minArray+= arr[i]
+        
+    }
+    
+    console.log(sum)
+
+    valuearr.innerHTML = sum 
+
+}
+
+sumArray([100,200])
+
